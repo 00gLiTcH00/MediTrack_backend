8e8028df50f26c9a8d1b178a1a5e3e4097d90818
Razvan Tinca <razvan.tinca@razvan.network>
2024-10-31 17:23:54 +0200

    stepper code

--- src/main/java/smartbits/junior/meditrack/config/WebConfig.java
+++ src/main/java/smartbits/junior/meditrack/config/WebConfig.java
@@ -1,34 +1,26 @@
 package smartbits.junior.meditrack.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
 
-@Configuration
-@RequiredArgsConstructor
-@EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
+//@Configuration
+//@RequiredArgsConstructor
+//@EnableWebSecurity
+//@EnableMethodSecurity(prePostEnabled = true)
 public class WebConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .cors(cors -> cors.configurationSource(request -> {
-                    var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
-                    corsConfiguration.setAllowedOrigins(List.of("*"));
-                    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "DELETE"));
-                    corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "Credentials"));
-                    corsConfiguration.setAllowCredentials(true);
-                    corsConfiguration.setExposedHeaders(List.of("Authorization"));
-                    return corsConfiguration;
-                }))
-                .build();
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .cors(cors -> cors.configurationSource(request -> {
+//                    var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+//                    corsConfiguration.setAllowedOrigins(List.of("*"));
+//                    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "DELETE"));
+//                    corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "Credentials"));
+//                    corsConfiguration.setAllowCredentials(true);
+//                    corsConfiguration.setExposedHeaders(List.of("Authorization"));
+//                    return corsConfiguration;
+//                }))
+//                .authorizeHttpRequests(req -> req.anyRequest().permitAll())
+//                .build();
+//    }
 }
